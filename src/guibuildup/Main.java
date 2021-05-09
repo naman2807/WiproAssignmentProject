632cdf5028a8cd7b26751f684c29139e4e7f6180
naman2807 <naman.agarwal_cs19@gla.ac.in>
2021-05-09 10:58:06 +0530

    Updated MainWindow.fxml file and build default start() and main() method in Main.java class

--- src/guibuildup/Main.java
+++ src/guibuildup/Main.java
@@ -1,5 +1,13 @@
 package guibuildup;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -8,5 +16,19 @@ package guibuildup;
  * Date: 09-05-2021
  */
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainWindow.fxml")));
+        primaryStage.setTitle("GLA University Exam Portal");
+        primaryStage.setScene(new Scene(root, 700, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
 }
