d5701550d41a6d69a230a32b42a11cba48e66f59
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-20 11:52:02 +0530

    Created a new class called ShowCandidatesController.java

---
+++ src/guibuildup/ShowCandidatesController.java
@@ -0,0 +1,12 @@
+package guibuildup;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: guibuildup
+ * Project Name: WiproAssignmentProject
+ * Date: 20-05-2021
+ */
+
+public class ShowCandidatesController {
+}
